f26380bffa7ecc71e8e1de356390484dc9e486fa
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-05-23 18:03:32 +0530

    negative indexes in strings in python

---
+++ strings in python/negative indexes/negativeindexes.py
@@ -0,0 +1,2 @@
+message = "I will always pray for them because Once they were the reason for my very first smile in morning but now They left me by saying that they have someone else"
+print(message[-10] + message[0])
