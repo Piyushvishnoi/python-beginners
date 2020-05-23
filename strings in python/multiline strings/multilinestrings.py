4f4b557200358bb4f3d427c2b54544abaa19280a
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-05-23 18:00:59 +0530

    multiline strings using triple colon

---
+++ strings in python/multiline strings/multilinestrings.py
@@ -0,0 +1,8 @@
+message = '''
+Hi Omji
+I just want to say not everyone in this world is good
+But Some people are really good and they will never ever change their attitude towards us
+But some people will leave uh for others..But u have to let them go from ur memories too and will always pray for their happiness
+I know this is really difficult But we have to do it but once they were our reason to smile everyday 
+but now they have someone else for their smile '''
+print(message)
