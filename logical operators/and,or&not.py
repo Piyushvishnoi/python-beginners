1f7ffde2b6a9bf152ff4405e1bf45032b7d0ce27
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-04 13:33:54 +0530

    Update and,or&not.py

--- logical operators/and,or&not.py
+++ logical operators/and,or&not.py
@@ -1,5 +1,5 @@
 get_salary = 125000
-good_life = True
+good_life = True 
 if get_salary > 100000 and good_life:
     print("To get job and work efficiently")
 else:
@@ -7,4 +7,4 @@ else:
 if get_salary > 100000 and not good_life:
     print("no means of living")
 elif get_salary < 100000 or good_life:
-    print("having a good life is better than higher salary")
+    print("having a good life is better than higher salary") 
