f4ec8a457b7f1fefb5e87bacf1b026cdcd7fa9e3
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-05 21:29:06 +0530

    weight conversion from lb to kg

---
+++ weight conversion/weightconverter.py
@@ -0,0 +1,10 @@
+weight = int(input("weight: "))
+unit = input("kilo(K) or lbs(L): ")
+if unit.upper() == "L":
+    convertedweight = weight * 0.45
+    print("Weight in kilo:" + str(convertedweight))
+elif unit.upper() == "K":
+    convertedweight = weight / 0.45
+    print("Weight in lbs:" + str(convertedweight))
+else:
+    print("Unit entered is wrong")
