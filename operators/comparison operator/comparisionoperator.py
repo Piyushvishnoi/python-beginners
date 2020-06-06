effc121ef5978d46d541aae70ec6ac1b90cc187e
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-06 23:41:51 +0530

    Update comparisionoperator.py

--- operators/comparison operator/comparisionoperator.py
+++ operators/comparison operator/comparisionoperator.py
@@ -1,8 +1,7 @@
 get_salary = 150000
 if get_salary >= 150000:
     print("sure to do the job")
- 
-elif get_salary > 70000 and get_salary < 125000:
+ elif get_salary > 70000 and get_salary < 125000:
     print("a chance of doing job")
 else:
     print("think and act according to situation")
