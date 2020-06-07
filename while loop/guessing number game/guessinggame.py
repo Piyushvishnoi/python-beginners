4f83fb3a42504c05cb88963d0430222e2c3d227b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-08 02:37:29 +0530

    guessing game

---
+++ while loop/guessing number game/guessinggame.py
@@ -0,0 +1,10 @@
+secret_number = 5
+guess_count = 0
+while guess_count < 3:
+    guess_number = int(input("guess_number: "))
+    guess_count += 1
+    if guess_number == secret_number:
+        print("You won !")
+        break
+else:
+    print("Sorry U have guessed it wrong !")
