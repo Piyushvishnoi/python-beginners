a3dc2305b93d8c32313e8d47ef1b37c1d0f5d710
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-08 14:20:10 +0530

    car game

---
+++ while loop/car game/cargame.py
@@ -0,0 +1,10 @@
+order = ""
+while order.upper() != "QUIT":
+    order = input("-> ")
+    if order.upper() == 'START':
+     print("Car started  Ready to Go!!")
+    elif order.upper() == 'STOP':
+     print("Car stopped!!")
+    elif order.
+    else:
+     print("command entered is wrong")
