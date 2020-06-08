ba758b695ef28251c0ca59232cec6eb77336587e
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-08 14:31:25 +0530

    Update cargame.py

--- while loop/car game/cargame.py
+++ while loop/car game/cargame.py
@@ -5,7 +5,7 @@ while order.upper() != "QUIT":
      print("Car started  Ready to Go!!")
     elif order.upper() == 'STOP':
      print("Car stopped!!")
-    elif order.upper() =='HELP':
+    elif order.upper() =='HELP': 
         print(''' 
         Start -> To start the car
         Stop-> To stop the car   
