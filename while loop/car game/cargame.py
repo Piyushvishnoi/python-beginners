09cbaf58bdb2614b6ba073521cab6605b8a82b26
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-08 14:28:46 +0530

    CAr game with help option

--- while loop/car game/cargame.py
+++ while loop/car game/cargame.py
@@ -2,9 +2,14 @@ order = ""
 while order.upper() != "QUIT":
     order = input("-> ")
     if order.upper() == 'START':
-     print("Car started  Ready to Go!!")     
-    elif order.upper() == 'STOP': 
+     print("Car started  Ready to Go!!")
+    elif order.upper() == 'STOP':
      print("Car stopped!!")
-    elif order.
-    else: 
+    elif order.upper() =='HELP':
+        print('''
+        Start -> To start the car
+        Stop-> To stop the car
+        Quit-> to quit from game
+        ''')
+    else:
      print("command entered is wrong")
