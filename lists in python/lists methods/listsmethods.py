c90f3d61765284f955c32e2419b321a64a20a87f
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-06-14 22:59:06 +0530

    lists methods

---
+++ lists in python/lists methods/listsmethods.py
@@ -0,0 +1,10 @@
+number = [1,2,3,'Piyush']
+number.append('Vishnoi')
+print(number)
+number.insert(1,'Omji')
+print(number)
+number.remove(2)
+print(number)
+empty = number.clear()
+print(empty)
+print(number)
