876341c29550b6ccc417e6031365e23734c08fda
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-07-11 01:18:46 +0530

    Update dictionarymethods.py

--- dictionaries/dictionary methods/dictionarymethods.py
+++ dictionaries/dictionary methods/dictionarymethods.py
@@ -3,7 +3,7 @@ student = {
     "class" : "Btech SEcond year", 
     "rollno" : "R171218071"  
 }  
-student["Sapid"] = 500067083  
+student["Sapid"] = 500067083   
 print(student["Sapid"])  
 student["name"] = "Piyush Vishnoi" 
 print(student["name"])
