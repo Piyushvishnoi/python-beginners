f7f3670b78f1713e4e553a05e1c74acdf416a383
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-07-24 23:39:54 +0530

    Update dictionarydemo.py

--- dictionaries/dictionary demo/dictionarydemo.py
+++ dictionaries/dictionary demo/dictionarydemo.py
@@ -3,6 +3,6 @@ student = {
     "class" : "Btech SEcond year",   
     "rollno" : "R171218071"       
 }   
-print(student["name"])      
+print(student["name"])       
 print(student.get("rollno"))
  
