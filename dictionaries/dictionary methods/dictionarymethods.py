b4b3086e69def062618922704a7197aeddf7e39b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-08-02 23:24:57 +0530

    Update dictionarymethods.py

--- dictionaries/dictionary methods/dictionarymethods.py
+++ dictionaries/dictionary methods/dictionarymethods.py
@@ -4,6 +4,6 @@ student = {
     "rollno" : "R171218071"    
 }   
 student["Sapid"] = 500067083   
-print(student["Sapid"])       
+print(student["Sapid"])        
 student["name"] = "Piyush Vishnoi"  
 print(student["name"])
