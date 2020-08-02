9d162b8ba4725ffc932d25b54216a63112701266
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-08-02 23:31:32 +0530

    Update dictionarymethods.py

--- dictionaries/dictionary methods/dictionarymethods.py
+++ dictionaries/dictionary methods/dictionarymethods.py
@@ -2,7 +2,7 @@ student = {
     "name" : "Piyush",
     "class" : "Btech SEcond year",    
     "rollno" : "R171218071"    
-}   
+}    
 student["Sapid"] = 500067083   
 print(student["Sapid"])        
 student["name"] = "Piyush Vishnoi"  
