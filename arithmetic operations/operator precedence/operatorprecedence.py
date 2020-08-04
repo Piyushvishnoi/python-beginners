c53f2074581d55f7eb77dd3ccb7f0305b8f2b9cd
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-08-04 22:59:31 +0530

    Update operatorprecedence.py

--- arithmetic operations/operator precedence/operatorprecedence.py
+++ arithmetic operations/operator precedence/operatorprecedence.py
@@ -1,4 +1,4 @@
 x = 10 + 3 * 2 ** 2
-print('Exponential operator have higher precedence ' + str(x))  
+print('Exponential operator have higher precedence ' + str(x))   
 y = (10 + 3) * 2 ** 2    
 print(y) 
