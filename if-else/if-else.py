9b612a90561b5b65e3e1be5503a2d427dcc7e33f
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-08-18 23:29:42 +0530

    Update if-else.py

--- if-else/if-else.py
+++ if-else/if-else.py
@@ -2,7 +2,7 @@ is_cold = True
 is_hot = False    
 if is_hot:    
     print("Very Hot")          
-elif is_cold: 
+elif is_cold:  
     print("Very cold")         
 else:       
     print("Not cold")      
