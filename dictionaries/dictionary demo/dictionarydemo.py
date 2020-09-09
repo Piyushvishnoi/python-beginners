0088c90410f7e38f6e5fb6e8577c4a57959ca261
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-09-09 23:34:49 +0530

    Update dictionarydemo.py

--- dictionaries/dictionary demo/dictionarydemo.py
+++ dictionaries/dictionary demo/dictionarydemo.py
@@ -2,6 +2,6 @@ student = {
     "name" : "Piyush",       
     "class" : "Btech SEcond year",             
     "rollno" : "R171218071"                   
-}            
+}             
 print(student["name"])         
 print(student.get("rollno"))   
