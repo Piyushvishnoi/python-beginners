23bc1b1792f509ff9cef0aa1016b9aac047c9688
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-10-07 23:40:37 +0530

    Update augmentedassignmentoperator.py

--- arithmetic operations/augmented assignment operator/augmentedassignmentoperator.py
+++ arithmetic operations/augmented assignment operator/augmentedassignmentoperator.py
@@ -1,7 +1,7 @@
 x = 10         
 x = x + 2          
 print(x)                                 
-x += 2                                    
+x += 2                                     
 print("By using augmented assignment operator:" + str(x))                
     
  
